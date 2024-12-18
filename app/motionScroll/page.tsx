c4025f1dae452093a6066bad13eb4105287ec385
gunhee0000 <132315752+gunhee0000@users.noreply.github.com>
2024-12-18 11:10:32 +0900

    dd

--- app/motionScroll/page.tsx
+++ app/motionScroll/page.tsx
@@ -1,51 +1,36 @@
 'use client';
 
-// import { animated,useScroll, useSpring } from "@react-spring/web";
-// import { useEffect, useState } from "react";
-
-// export default function MotionScroll() {
-//     const { scrollYProgress } = useScroll();
-//     const [ren, setRen] = useState(false);
-    
-//       useEffect(() => {
-//         setRen(true);
-//       }, []);
-
-//     const textStyle = useSpring({
-//         y: scrollYProgress.to([0.9, 1], [100, 0]),
-//         opacity: scrollYProgress.to([0.7, 1], [0, 100]),
-//     });
-
-//     if(!ren) return null;
-    
-//     return(
-//         <div className="w-screen h-[2000px] flex justify-center items-center">
-//             <animated.div
-//                 className="fixed top-0 left-0 w-full h-full bg-orange-400"
-//                 style={{
-//                     clipPath: scrollYProgress.to(val => `circle(${val * 100}%)`),
-//                 }}>
-//                 <h1 className="text-blue-700 text-5xl pl-8 pt-8 font-bold">
-//                     <span>
-//                         <animated.span className="block" style={textStyle}>Aha!</animated.span>
-//                     </span>
-//                     <span>
-//                         <animated.span className="block" style={textStyle}>You found me!</animated.span>
-//                     </span>
-//                 </h1>
-//             </animated.div>
-//         </div>
-//     );
-// }
-
-import { motion, useTransform, useScroll } from "framer-motion";
+import { motion, useTransform, useScroll, useAnimation } from "framer-motion";
+import { useEffect } from "react";
 
 export default function MotionScroll() {
     const {scrollYProgress} = useScroll();
 
     const scale = useTransform(scrollYProgress, [0, 1], [0, 3]);
-    const opacity = useTransform(scrollYProgress, [0.9, 1], [0, 1]);
-    const y = useTransform(scrollYProgress, [0.7, 1], [100, 0]);
+    // const opacity = useTransform(scrollYProgress, [0.9, 1], [0, 1]);
+    // const y = useTransform(scrollYProgress, [0.7, 1], [100, 0]);
+
+    const textApi = useAnimation();
+
+    useEffect(() => {
+        const api = scrollYProgress.on("change", (latest) => {
+          if (latest > 0.7) {
+            textApi.start({
+              y: "0%",
+              opacity: 1,
+              transition: { duration: 0.1 },
+            });
+          } else {
+            textApi.start({
+              y: "100%",
+              opacity: 0,
+              transition: { duration: 0.1 },
+            });
+          }
+        });
+    
+        return () => api();
+      }, [scrollYProgress, textApi]);
 
     return(
         <div className="w-screen h-[2000px] flex justify-center items-center">
@@ -59,9 +44,15 @@ export default function MotionScroll() {
                     scale,
                 }}
             />
-            <div className="fixed top-0 left-0 flex flex-col pt-8 pl-8 text-5xl font-bold text-blue-700">
-                <motion.span style={{opacity, y}}>Aha!</motion.span>
-                <motion.span style={{opacity, y}}>You found me!</motion.span>
+            <div className="fixed top-0 left-0 flex flex-col mt-8 ml-8 text-5xl font-bold text-blue-700 gap-3">
+                <motion.span
+                    initial={{ opacity: 0, }}
+                    animate={textApi}
+                >Aha!</motion.span>
+                <motion.span 
+                    initial={{ opacity: 0, }}
+                    animate={textApi}
+                >You found me!</motion.span>
             </div>
         </div>
     )
